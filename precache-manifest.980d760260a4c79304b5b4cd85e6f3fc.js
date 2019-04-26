601cd1dca1aa926cf85ab35f800da9b072f0e671
SeongChan Lee <sclee@MacBook-Pro-3.local>
2019-04-26 17:15:44 +0900

    Updates

---
+++ precache-manifest.980d760260a4c79304b5b4cd85e6f3fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c668ad59fc53c0b52d178fe918ddf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b18f742998783b0d44a0",
+    "url": "/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b18f742998783b0d44a0",
+    "url": "/static/js/main.f3d20195.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
